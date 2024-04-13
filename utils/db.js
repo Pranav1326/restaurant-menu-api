31fe3d7349656d4723fed685ce31a938a18fdf5e
Pranav1326 <visavadiapa@gmail.com>
2024-04-13 22:04:03 +0530

    user login update

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 
-mongoose.connect(`mongodb+srv://${process.env.MONGODB_PROFILE}:${process.env.MONGODB_PASSWORD}@cluster0.o0lxkh1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+mongoose.connect(`mongodb+srv://${process.env.MONGODB_PROFILE}:${process.env.MONGODB_PASSWORD}@cluster0.o0lxkh1.mongodb.net/restarurant?retryWrites=true&w=majority&appName=Cluster0`)
 .then(res => {
     console.log("MongoDB Connected!");
 })
